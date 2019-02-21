7059c79e10abbef7d01e99de110d51b58b76d44e
Muhammad Khurram Khan <mkk783@g.harvard.edu>
2019-02-20 22:28:14 -0500

    added index.php file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>John Gosier</title>
+	<meta charset="utf-8">
+
+
+</head>
+<body>
+
+	<div class="container">
+
+		<h1>Muhammad Khurram Khan</h1>
+
+		<img src='images/KK.jpg' alt='Khurram Khan'>
+
+		<h2>About Me</h2>
+		<p>
+			My name is Muhammad Khurram Khan. You can call me Khurram. I'm a Lead Application Engineer in a tech company. 
+		</p>
+
+		<h2>Random Quote</h2>
+		<blockquote>
+			To find yourself, think for yourself. -Socrates		</blockquote>
+
+	</div>
+
+
+</body>
+</html>
